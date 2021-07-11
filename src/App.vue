cf40a3a3055d2e33b182aeb922df9a12ab82f9d6
Pasha <pasha.colocov@gmail.com>
2021-07-11 13:17:26 +0300

    start

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>count of likes: {{ likes }}</div>
+    <div>count of dislikes: {{ dislikes }}</div>
+    <button @click="likes++">likes</button>
+    <button @click="dislikes++">dislikes</button>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            likes: 0,
+            dislikes: 0
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
